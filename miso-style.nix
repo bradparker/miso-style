d294e9bf3e125b2378b5dfeb4d5d5c69c3e46591
Brad Parker <hi@bradparker.com>
2018-02-17 17:06:47 +1000

    Add missing dlist dep in nix file

--- miso-style.nix
+++ miso-style.nix
@@ -1,12 +1,12 @@
-{ mkDerivation, base, containers, hashable, hspec, miso, stdenv
-, text, transformers
+{ mkDerivation, base, containers, dlist, hashable, hspec, miso
+, stdenv, transformers
 }:
 mkDerivation {
   pname = "miso-style";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    base containers hashable miso text transformers
+    base containers dlist hashable miso transformers
   ];
   testHaskellDepends = [ base hspec miso ];
   description = "CSS in Haskell for the Miso framework";
