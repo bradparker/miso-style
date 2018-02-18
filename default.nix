d47ec288f0fe92390d9dab5ecd481b63057496a0
Brad Parker <hi@bradparker.com>
2018-02-18 22:44:00 +1000

    Attempt to tidy up Nix setup a little
    Lock example NixPkgs to an LTS commit, remove NixPkgs pinning from root.

--- default.nix
+++ default.nix
@@ -5,13 +5,4 @@
     sha256 = "04dgg0f2839c1kvlhc45hcksmjzr8a22q1bgfnrx71935ilxl33d";
   }){}
 }:
-let
-  misoPkg = import (pkgs.fetchFromGitHub {
-    owner = "dmjio";
-    repo = "miso";
-    sha256 = "18jhr1ihf0vwwvp134j24isvzq699x5iy7l9ihrah760zmcxi7d2";
-    rev = "e3d3d874337a4a44adc4b6bdb8b18d907c6c1e34";
-  }) {};
-in pkgs.haskell.packages.ghc802.callPackage ./miso-style.nix {
-  miso = misoPkg.miso-ghc;
-}
+pkgs.haskell.packages.ghc802.callPackage ./miso-style.nix {}
--- examples/hello-world/default.nix
+++ examples/hello-world/default.nix
@@ -1,21 +1,22 @@
 { pkgs ? import ((import <nixpkgs> {}).fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "a0aeb23";
-    sha256 = "04dgg0f2839c1kvlhc45hcksmjzr8a22q1bgfnrx71935ilxl33d";
+    rev = "1c988f97e9910665d258a8b83437178349b4e624";
+    sha256 = "10vmr1r172bgv5wqpram08pd657ygdcpm1gr88sdj1caqwxbp5ac";
   }){}
 }:
 let
-  misoPkg = import (pkgs.fetchFromGitHub {
+  haskellPackages = pkgs.haskell.packages.ghcjs;
+  miso = import (pkgs.fetchFromGitHub {
     owner = "dmjio";
     repo = "miso";
     sha256 = "18jhr1ihf0vwwvp134j24isvzq699x5iy7l9ihrah760zmcxi7d2";
     rev = "e3d3d874337a4a44adc4b6bdb8b18d907c6c1e34";
   }) {};
-  misoStyle = pkgs.haskell.packages.ghcjs.callPackage ../../miso-style.nix {
-    miso = misoPkg.miso-ghcjs;
+  miso-style = haskellPackages.callPackage ../../miso-style.nix {
+    miso = miso.miso-ghcjs;
   };
-in pkgs.haskell.packages.ghcjs.callPackage ./hello-world.nix {
-  miso = misoPkg.miso-ghcjs;
-  miso-style = misoStyle;
+in haskellPackages.callPackage ./hello-world.nix {
+  miso = miso.miso-ghcjs;
+  miso-style = miso-style;
 }
